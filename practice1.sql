861fe9a4adb91f2e2e21b9094bae806044113808
Hossein Araghi <hoseinaraghi84@gmail.com>
2024-11-22 12:09:22 +0330

    foreign key

--- practice1.sql
+++ practice1.sql
@@ -1,9 +1,22 @@
-CREATE TABLE users
+create table users
 (
-    id         BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    name       VARCHAR(255),
-    family     VARCHAR(255),
-    email      VARCHAR(50),
-    password   VARCHAR(255),
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    id         bigint unsigned auto_increment primary key,
+    first_name VARCHAR(255)           not null,
+    last_name  VARCHAR(255)           not null,
+    email      VARCHAR(50)            not null,
+    password   VARCHAR(255)           not null,
+    biography  longtext,
+    role       enum ('user', 'admin') not null default 'user',
+    created_at timestamp                       default current_timestamp
+);
+
+create table posts
+(
+    id         bigint unsigned auto_increment primary key,
+    user_id    bigint unsigned,
+    title      varchar(255) not null,
+    body       longtext     not null,
+    created_at timestamp default current_timestamp,
+
+    foreign key (user_id) references users(id)
 );
